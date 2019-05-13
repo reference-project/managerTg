8423cb0c6f9022f674c802ee40980d54777fd21e
myguilong <1360747193@qq.com>
2019-05-13 17:25:16 +0800

    商品购物车等

--- src/App.vue
+++ src/App.vue
@@ -192,7 +192,9 @@
                      <router-link to="/listFoods" class="a"> <i class="am-icon-bar-chart sidebar-nav-link-logo"></i>商品列表</router-link>
                 
                 </li>
-
+         <li class="sidebar-nav-link">
+                     <router-link to="/addCategory" class="a"> <i class="am-icon-bar-chart sidebar-nav-link-logo"></i>增加分类</router-link>
+                </li>
                 <li class="sidebar-nav-heading">Page<span class="sidebar-nav-heading-info"> 常用页面</span></li>
                 <li class="sidebar-nav-link">
                     <a href="javascript:;" class="sidebar-nav-sub-title">
